f019f703458c6aa98af872bd834c9d938fbb1c1b
Bobby Gray <finan.phillips@gmail.com>
2023-10-21 19:43:30 -0500

    added a recursive script to dump all rules by .yml to file.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from collections.abc import MutableMapping
 import json
 from numpy import column_stack
 import pandas as pd 
@@ -7,16 +8,23 @@ import requests
 import yaml
 
 def test():
-    with open('sigma_dict1.json') as rules:
+    with open('sigma_dict_pretty.json') as rules:
         rules0 = rules.read()
         rules1 = json.loads(rules0)
-
-        with open('sigma_dict_pretty.json', 'w') as fp:
-            json.dump(rules1, fp, indent=2)
-                                    
-
-
-
+        gcp_rules_methods = {}
+        for k, v in rules1.items():
+            if k == "cloud":
+                for k0, v0 in v.items():
+                    if k0 == "gcp":
+                        for k1, v1 in v0.items():
+                            if k1 == "audit":
+                                for k2, v2 in v1.items():
+                                    detection = v2.get("detection")
+                                    selection = detection.get("selection")
+                                    tags = v2.get("tags")
+                                    for k3, v3, in selection.items():
+                                        if str(k3).endswith("|startswith"):
+                                            print(k3)        
 
 
 if __name__=="__main__":
