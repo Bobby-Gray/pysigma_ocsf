05877fc53e7d9571bc43f5c0a36325601d659d80
Bobby Gray <finan.phillips@gmail.com>
2023-11-06 21:18:22 -0600

    Added comments to dump_rules_with_ocsf_pipeline.py test script detailing usage.

--- pipelines/ocsf/dump_rules_with_ocsf_pipeline.py
+++ pipelines/ocsf/dump_rules_with_ocsf_pipeline.py
@@ -4,14 +4,20 @@ from sigma.collection import SigmaCollection
 import os 
 import json
 
+# Test script utilizing the ocsf pipeline and splunk backend. 
+# This script will dump a json dictionary file, sigma_rules_dict.json, of rule names with their respective directory paths as keys and splunk searches as the values. 
+#   Configured to be ran from within the same folder as sigma_ocsf.py
+#   Requires a rules directory containing sigma rules. 
+#   To limit to a specific directory/path, include an if statement for i before the try loop. 
+#       e.g., if str(i).startswith("rules/cloud"):
+#   Defaults to "rules" and will prepend any containing folder structure to the rule name.
+
 pipeline = ocsf_pipeline()
 backend = SplunkBackend(pipeline)
 
 sigma_rules_dict = {}
-
 def dump_rules():
     repo_path = input("Input sigma rule path: ") or "rules"
-    # replace_dict = input("Input replace dictionary containing key, value pairs of logsource and field mapping data: ") or "replace_dict.json"
     filenames = []
     for root, dirs, files in os.walk(repo_path):
         for filename in files:
@@ -19,7 +25,6 @@ def dump_rules():
         for dirname in dirs:
             filenames.append((os.path.join(root, dirname)))
     for i in filenames:
-        if str(i).startswith("rules/cloud"):
             try:
                 with open(""+i, "r") as ry: 
                     rules = SigmaCollection.from_yaml(ry)
